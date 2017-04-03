9dfde507aaa3197cc5fdb6e6dc841d608730b4f8
Paul Water <p.r.water@setbased.nl>
2017-04-03 11:08:12 +0200

    Updated dependencies.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,12 +1,5 @@
 <?php
-
-use SetBased\Affirm\ErrorHandler;
-
 error_reporting(E_ALL);
 date_default_timezone_set('Europe/Amsterdam');
 
 require __DIR__.'/../vendor/autoload.php';
-
-// Set the error handler.
-$handler = new ErrorHandler();
-$handler->register(false);
