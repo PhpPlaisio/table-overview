380170a5b21d25090fcdca1d15a0119638a1d527
Paul Water <p.r.water@setbased.nl>
2017-06-13 16:50:20 +0200

    Added class HyperLinkTableColumn.

---
+++ src/TableColumn/HyperLinkTableColumn.php
@@ -0,0 +1,55 @@
+<?php
+//----------------------------------------------------------------------------------------------------------------------
+namespace SetBased\Abc\Table\TableColumn;
+
+use SetBased\Abc\Helper\Html;
+
+//----------------------------------------------------------------------------------------------------------------------
+/**
+ * Table column for table cells with a hyper link.
+ */
+class HyperLinkTableColumn extends TableColumn
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * The field name of the data row used for generating this table column.
+   *
+   * @var string
+   */
+  protected $fieldName;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Object constructor.
+   *
+   * @param string|int|null $headerText The header text of this table column.
+   * @param string          $fieldName  The key to be used for getting the value from the data row.
+   */
+  public function __construct($headerText, $fieldName)
+  {
+    parent::__construct('text');
+
+    $this->headerText = $headerText;
+    $this->fieldName  = $fieldName;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * {@inheritdoc}
+   */
+  public function getHtmlCell($row)
+  {
+    $url = $row[$this->fieldName];
+
+    if ($url===null)
+    {
+      return '<td></td>';
+    }
+
+    return '<td>'.Html::generateElement('a', ['href'=> $url], $url).'</td>';
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
