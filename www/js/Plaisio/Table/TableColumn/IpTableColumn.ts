4270286f963003a7b1d84f0d986ecfff9a381d0b
Paul Water <p.r.water@setbased.nl>
2021-05-13 12:38:03 +0200

    Fix sorting of IP table column.

--- www/js/Plaisio/Table/TableColumn/IpTableColumn.ts
+++ www/js/Plaisio/Table/TableColumn/IpTableColumn.ts
@@ -1,16 +1,25 @@
 import {OverviewTable} from '../OverviewTable';
-import {NumberTableColumn} from './NumberTableColumn';
+import {TextTableColumn} from './TextTableColumn';
 
 /**
  * Table column for IPv4 and IPv6 addresses.
  */
-export class IpTableColumn extends NumberTableColumn
+export class IpTableColumn extends TextTableColumn
 {
-  // Nothing to implement.
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * @inheritDoc
+   */
+  public getSortKey(tableCell: HTMLTableCellElement): any
+  {
+    return $(tableCell).attr('data-value') || $(tableCell).text();
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 OverviewTable.registerTableColumn('ip', IpTableColumn);
 OverviewTable.registerTableColumn('ipv4', IpTableColumn);
 
-// Plaisio\Console\Helper\TypeScript\TypeScriptMarkHelper::md5: 5091fbb445303c3af6653bcbea4134b3
+// Plaisio\Console\Helper\TypeScript\TypeScriptMarkHelper::md5: bef1c36e1509e3ea655fbd242600da23
