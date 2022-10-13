d262f5eab4d4fb98dea29e4a4d89fc99cd6b118d
Paul Water <p.r.water@setbased.nl>
2022-10-13 08:05:06 +0200

    Rewrite of imports.

--- www/js/Plaisio/Table/OverviewTablePackage.ts
+++ www/js/Plaisio/Table/OverviewTablePackage.ts
@@ -1,15 +1,15 @@
-import './TableColumn/BoolTableColumn';
-import './TableColumn/DateTimeTableColumn';
-import './TableColumn/IpTableColumn';
-import './TableColumn/NoneTableColumn';
-import './TableColumn/NumberTableColumn';
-import './TableColumn/TextTableColumn';
-import './TableColumn/UuidTableColumn';
-import {OverviewTable} from './OverviewTable';
+import 'Plaisio/Table/TableColumn/BoolTableColumn';
+import 'Plaisio/Table/TableColumn/DateTimeTableColumn';
+import 'Plaisio/Table/TableColumn/IpTableColumn';
+import 'Plaisio/Table/TableColumn/NoneTableColumn';
+import 'Plaisio/Table/TableColumn/NumberTableColumn';
+import 'Plaisio/Table/TableColumn/TextTableColumn';
+import 'Plaisio/Table/TableColumn/UuidTableColumn';
+import {OverviewTable} from 'Plaisio/Table/OverviewTable';
 
 export class OverviewTablePackage extends OverviewTable
 {
   // Nothing to implement.
 }
 
-// Plaisio\Console\Helper\TypeScript\TypeScriptMarkHelper::md5: 2ccf52106b9de235907707cef554cfdb
+// Plaisio\Console\Helper\TypeScript\TypeScriptMarkHelper::md5: 0da8b3912e0fdeeb05f78111262236e3
